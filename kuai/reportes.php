0616bcd178cd08f22db7d8f29e2adf7d2adfc08d
Programador <programador@localhost.localdomain>
2016-02-26 04:45:09 -0430

    arreglo de docente

--- kuai/reportes.php
+++ kuai/reportes.php
@@ -3,6 +3,10 @@
 	$resultadoP= pg_query($conexion, $sqlP) or die("Error en la Consulta SQL poi");
 	$query = "SELECT * FROM edificio a INNER JOIN sede b ON b.id = a.id_sede  ORDER BY  a.id_sede, a.edificio ASC";
             $resultado = pg_query($conexion, $query) or die("Error en la Consulta SQL");
+                            $q111 = "SELECT * FROM persona a 
+                            INNER JOIN profesor b ON a.cedula = b.cedula ORDER BY a.nombre ASC";
+                            $res11 = pg_query($conexion, $q111) ;
+        
 
 ?>
 <div id="reportes">
@@ -16,7 +20,14 @@
 		
 		<div>
             <label>Profesor</label><br>
-            <input id="prof" name="prof">
+            <select id="prof"  name="prof">
+                                <option value=""        >-------------------------</option>
+                                <?php while ($row11 = pg_fetch_row($res11)) { ?>
+                                <option value='<?= $row11[0]?>' > <?= $row11[1]." ".$row11[3]?> </option>
+                            <?php } ?>
+                             </select>
+            <!--<input id="profA" name="profA" value= "<?php echo $yasmin;?>">-->
+
             
             <button class="btn btn-primary btn-xs" onclick="consultaProfesor('profesor','prof')">consultar</button>
 
@@ -73,5 +84,6 @@
         </select>
     </div>
 </div>
+
 
 <div id="impreporte"></div>
